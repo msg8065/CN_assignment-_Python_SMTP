9ce071c4a4e93027ebd27adf9979895ed3b88f29
msg8065 <msg8065@nyu.edu>
2024-09-24 02:24:07 +0530

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -7,33 +7,33 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((mailserver,port))
-    clientSocket.recv(1024).decode()
+    clientSocket.recv(1111).decode()
     # Send HELO command
     heloCommand = 'HELO Mike\r\n'
     clientSocket.send(heloCommand.encode())
-    clientSocket.recv(1024).decode()
+    clientSocket.recv(1111).decode()
     # Send MAIL FROM command
     mailCommand = 'MAIL FROM:<from@local.host\r\n'
     clientSocket.send(mailCommand.encode())
-    clientSocket.recv(1024).decode()
+    clientSocket.recv(1111).decode()
     # Send RCPT TO command
     rcptCommand = 'RCPT TO:<rcpt@local.host\r\n'
     clientSocket.send(rcptCommand.encode())
-    clientSocket.recv(1024).decode()
+    clientSocket.recv(1111).decode()
     # Send DATA command
     dataCommand = 'DATA\r\n\r\n'
     clientSocket.send(dataCommand.encode())
-    clientSocket.recv(1024).decode()
+    clientSocket.recv(1111).decode()
     # Send message data
     emailData = "Subject: Sample mail\r\n" + "From: from@local.host\r\n" + "To: rcpt@local.host" + msg
     clientSocket.send(emailData.encode())
     # Message ends with a single period, send message end
     clientSocket.send(endmsg.encode())
-    clientSocket.recv(1024).decode()
+    clientSocket.recv(1111).decode()
     # Send QUIT command
     quitCommand = 'QUIT\r\n'
     clientSocket.send(quitCommand.encode())
-    clientSocket.recv(1024).decode()
+    clientSocket.recv(1111).decode()
     # Close socket
     clientSocket.close()
 
